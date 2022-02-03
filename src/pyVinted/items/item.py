246ba7ac5da93e9dd4348f8d5ccf1c5f87ab3837
Aimé Risson <aime.risson.1@gmail.com>
2022-02-03 08:13:12 +0100

    adding functionality isNewItem

--- src/pyVinted/items/item.py
+++ src/pyVinted/items/item.py
@@ -1,3 +1,5 @@
+from datetime import datetime, timezone
+
 class Item:
     def __init__(self, data):
         self.id = data['id']
@@ -7,9 +9,13 @@ class Item:
         self.brand = data['brand']
         self.currency = data['currency']
         self.price_numeric = data['price_numeric']
-        self.created_at_ts = data['created_at_ts']
-        self.updated_at_ts = data['updated_at_ts']
+        self.created_at_ts = datetime.fromisoformat(data['created_at_ts'])
+        self.updated_at_ts = datetime.fromisoformat(data['updated_at_ts'])
         self.photos = [ photo['full_size_url'] for photo in data['photos']]
         self.city = data['city']
         self.country = data['country']
         self.status_id = data['status_id']
+    
+    def isNewItem(self, delay=3):
+        delta = datetime.now(timezone.utc) - self.updated_at_ts
+        return delta.seconds < delay*60
