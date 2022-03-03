8e7f8f74cb76eb35e793d0bde7c3f899a960f823
Aimé Risson <aime.risson.1@gmail.com>
2022-03-03 06:57:35 +0100

    fixing vinted new api

--- src/pyVinted/items/item.py
+++ src/pyVinted/items/item.py
@@ -5,18 +5,11 @@ class Item:
     def __init__(self, data):
         self.id = data['id']
         self.title = data['title']
-        self.description = data['description']
-        self.size = data['size']
-        self.brand = data['brand']
+        self.brand_title = data['brand_title']
         self.currency = data['currency']
-        self.price_numeric = data['price_numeric']
-        self.created_at_ts = datetime.fromisoformat(data['created_at_ts'])
-        self.updated_at_ts = datetime.fromisoformat(data['updated_at_ts'])
-        self.photos = [ photo['full_size_url'] for photo in data['photos']]
-        self.city = data['city']
-        self.country = data['country']
-        self.status_id = data['status_id']
-        self.url = f"{requester.VINTED_URL}{data['path']}"
+        self.price = data['price']
+        self.photo = data['photo']['url']
+        self.url = data['url']
     
     def isNewItem(self, minutes=3):
         delta = datetime.now(timezone.utc) - self.created_at_ts
