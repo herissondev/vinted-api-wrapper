8277bc586ddef3cac633c9ef74e0989411eb4c5a
Aimé Risson <aime.risson.1@gmail.com>
2022-03-21 19:56:49 +0100

    adding proxy option and file reformat

--- src/pyVinted/items/item.py
+++ src/pyVinted/items/item.py
@@ -1,19 +1,22 @@
 from datetime import datetime, timezone
 from pyVinted.requester import requester
- 
+
+
 class Item:
     def __init__(self, data):
-        self.id = data['id']
-        self.title = data['title']
-        self.brand_title = data['brand_title']
+        self.id = data["id"]
+        self.title = data["title"]
+        self.brand_title = data["brand_title"]
         self.size_title = data["size_title"]
-        self.currency = data['currency']
-        self.price = data['price']
-        self.photo = data['photo']['url']
-        self.url = data['url']
-        self.created_at_ts = datetime.fromtimestamp(data['photo']['high_resolution']["timestamp"], tz=timezone.utc)
-    
+        self.currency = data["currency"]
+        self.price = data["price"]
+        self.photo = data["photo"]["url"]
+        self.url = data["url"]
+        self.created_at_ts = datetime.fromtimestamp(
+            data["photo"]["high_resolution"]["timestamp"], tz=timezone.utc
+        )
+
     def isNewItem(self, minutes=3):
         delta = datetime.now(timezone.utc) - self.created_at_ts
         print(delta)
-        return delta.seconds < minutes*60
+        return delta.seconds < minutes * 60
--- src/pyVinted/vinted.py
+++ src/pyVinted/vinted.py
@@ -1,41 +1,30 @@
-from urllib.parse import urlparse,parse_qsl
+from urllib.parse import urlparse, parse_qsl
 import requests
 from .items.item import Item
 from pyVinted.items import Items
 from pyVinted.requester import requester
 
+
 class Vinted:
-    '''
+    """
     This class is built to connect with the pyVinted API.
 
     It's main goal is to be able to retrieve items from a given url search.\n
     The items are then be returned in a json format
-    '''
+    """
 
-    def __init__(self, domain = "fr"):
+    def __init__(self, domain="fr", proxy=None):
         """
         Args:
             domain (str): Domain to be used, example: "fr" for France, "de" for Germany...
-            
+
         """
+        if proxy is not None:
+            requester.session.proxies.update(proxy)
+
         requester.setCookies(domain)
         self.items = Items()
-    
+
     # def login(self,username,password):
     #     requester.login(username=username, password=password)
     #     requester.message()
-        
-
-    
-
-    
-        
-
-    
-
-    
-
-
-    
-        
-    
