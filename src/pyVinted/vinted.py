928db2e162294fff62b84873dc7409634b05acfb
Aimé Risson <aime.risson.1@gmail.com>
2022-04-25 17:36:37 +0200

    removing domain

--- src/pyVinted/vinted.py
+++ src/pyVinted/vinted.py
@@ -10,25 +10,18 @@ class Vinted:
     This class is built to connect with the pyVinted API.
 
     It's main goal is to be able to retrieve items from a given url search.\n
-    The items are then returned in a json format
+
     """
 
-    def __init__(self, domain="fr", proxy=None, gateway=None):
+    def __init__(self, proxy=None):
         """
         Args:
-            domain (str): Domain to be used, example: "fr" for France, "de" for Germany...
+            Proxy : proxy to be used to bypass vinted's limite rate
 
         """
 
         if proxy is not None:
             requester.session.proxies.update(proxy)
-
-        elif gateway is not None:
-            requester.setCookies(domain=domain,gateway=gateway)
-            #requester.session.mount("https://www.vinted.fr", gateway)
-        else:
-            requester.setCookies(domain=domain)
-
 
         self.items = Items()
 
