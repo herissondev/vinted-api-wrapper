ffadf36bfc9f4e6a1afa8cea594adae6fc97cae6
Aimé Risson <aime.risson.1@gmail.com>
2024-07-03 14:56:15 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import setuptools
 
 
-VERSION = "0.4.9"
+VERSION = "0.4.10"
 DESCRIPTION = "Vinted API wrapper for python"
 
 with open("README.md", "r", encoding="utf-8") as fh:
